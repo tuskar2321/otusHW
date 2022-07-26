f6536743192b2e2836d2539ecebaf339a8c70e65
tuskar2321 <60214822+tuskar2321@users.noreply.github.com>
2022-07-26 23:45:57 +0300

    Update Test2.java
    qwerty

--- Test2.java
+++ Test2.java
@@ -4,25 +4,24 @@ public class Test2 {
     public static void main(String[] args) {
         int queResult = 0;
         String[][] que = new String[3][5];
-        que[0][0] = "В файл с каким расширением компилируется java-файл?";
+        que[0][0] = "Г‚ ГґГ Г©Г« Г± ГЄГ ГЄГЁГ¬ Г°Г Г±ГёГЁГ°ГҐГ­ГЁГҐГ¬ ГЄГ®Г¬ГЇГЁГ«ГЁГ°ГіГҐГІГ±Гї java-ГґГ Г©Г«?";
         que[0][1] = "1. cs";
         que[0][2] = "2. java";
         que[0][3] = "3. class";
         que[0][4] = "4. exe";
-        que[1][0] = "С помощью какой команды git можно получить полную копию удаленного репозитория?";
+        que[1][0] = "Г‘ ГЇГ®Г¬Г®Г№ГјГѕ ГЄГ ГЄГ®Г© ГЄГ®Г¬Г Г­Г¤Г» git Г¬Г®Г¦Г­Г® ГЇГ®Г«ГіГ·ГЁГІГј ГЇГ®Г«Г­ГіГѕ ГЄГ®ГЇГЁГѕ ГіГ¤Г Г«ГҐГ­Г­Г®ГЈГ® Г°ГҐГЇГ®Г§ГЁГІГ®Г°ГЁГї?";
         que[1][1] = "1. commit";
         que[1][2] = "2. push";
         que[1][3] = "3. clone";
         que[1][4] = "4. copy";
-        que[2][0] = "Какой применяется цикл, когда неизвестно кол-во итераций?";
+        que[2][0] = "ГЉГ ГЄГ®Г© ГЇГ°ГЁГ¬ГҐГ­ГїГҐГІГ±Гї Г¶ГЁГЄГ«, ГЄГ®ГЈГ¤Г  Г­ГҐГЁГ§ГўГҐГ±ГІГ­Г® ГЄГ®Г«-ГўГ® ГЁГІГҐГ°Г Г¶ГЁГ©?";
         que[2][1] = "1. while";
         que[2][2] = "2. for";
         que[2][3] = "3. loop";
         que[2][4] = "4. other";
 
         int correctAnswersCounter = printQueAndCheckAns(que, queResult);
-        System.out.println("Тест завершён! \nРезультат: Правильно - " + correctAnswersCounter + ". Неправильно - " + (3 - correctAnswersCounter));
-
+        System.out.println("Г’ГҐГ±ГІ Г§Г ГўГҐГ°ГёВёГ­! \nГђГҐГ§ГіГ«ГјГІГ ГІ: ГЏГ°Г ГўГЁГ«ГјГ­Г® - " + correctAnswersCounter + ". ГЌГҐГЇГ°Г ГўГЁГ«ГјГ­Г® - " + (3 - correctAnswersCounter));
     }
 
     public static Integer printQueAndCheckAns(String[][] que, int queResult) {
@@ -47,11 +46,11 @@ public class Test2 {
         Scanner sc = new Scanner(System.in);
         int answer = sc.nextInt();
         if (answer == correctAnswer) {
-            System.out.println("Правильно!");
+            System.out.println("ГЏГ°Г ГўГЁГ«ГјГ­Г®!");
             return true;
         }
         else {
-            System.out.println("Неправильно!");
+            System.out.println("ГЌГҐГЇГ°Г ГўГЁГ«ГјГ­Г®!");
             return false;
         }
     }
