14166f670706f45113f14ce24db72ad73d9bd488
tuskar2321 <60214822+tuskar2321@users.noreply.github.com>
2022-07-26 23:47:36 +0300

    Update Test2.java
    test, ver2

--- Test2.java
+++ Test2.java
@@ -4,24 +4,24 @@ public class Test2 {
     public static void main(String[] args) {
         int queResult = 0;
         String[][] que = new String[3][5];
-        que[0][0] = "Â ôàéë ñ êàêèì ðàñøèðåíèåì êîìïèëèðóåòñÿ java-ôàéë?";
+        que[0][0] = "В файл с каким расширением компилируется java-файл?";
         que[0][1] = "1. cs";
         que[0][2] = "2. java";
         que[0][3] = "3. class";
         que[0][4] = "4. exe";
-        que[1][0] = "Ñ ïîìîùüþ êàêîé êîìàíäû git ìîæíî ïîëó÷èòü ïîëíóþ êîïèþ óäàëåííîãî ðåïîçèòîðèÿ?";
+        que[1][0] = "С помощью какой команды git можно получить полную копию удаленного репозитория?";
         que[1][1] = "1. commit";
         que[1][2] = "2. push";
         que[1][3] = "3. clone";
         que[1][4] = "4. copy";
-        que[2][0] = "Êàêîé ïðèìåíÿåòñÿ öèêë, êîãäà íåèçâåñòíî êîë-âî èòåðàöèé?";
+        que[2][0] = "Какой применяется цикл, когда неизвестно кол-во итераций?";
         que[2][1] = "1. while";
         que[2][2] = "2. for";
         que[2][3] = "3. loop";
         que[2][4] = "4. other";
 
         int correctAnswersCounter = printQueAndCheckAns(que, queResult);
-        System.out.println("Òåñò çàâåðø¸í! \nÐåçóëüòàò: Ïðàâèëüíî - " + correctAnswersCounter + ". Íåïðàâèëüíî - " + (3 - correctAnswersCounter));
+        System.out.println("Тест завершён! \nРезультат: Правильно - " + correctAnswersCounter + ". Неправильно - " + (3 - correctAnswersCounter));
     }
 
     public static Integer printQueAndCheckAns(String[][] que, int queResult) {
@@ -46,11 +46,11 @@ public class Test2 {
         Scanner sc = new Scanner(System.in);
         int answer = sc.nextInt();
         if (answer == correctAnswer) {
-            System.out.println("Ïðàâèëüíî!");
+            System.out.println("Правильно!");
             return true;
         }
         else {
-            System.out.println("Íåïðàâèëüíî!");
+            System.out.println("Неправильно!");
             return false;
         }
     }
