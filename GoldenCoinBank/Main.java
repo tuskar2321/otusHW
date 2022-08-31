97d56958bc11c752426160685c0a4a294b51ad92
tuskar2321 <tuskar2321@mail.ru>
2022-09-01 00:20:43 +0300

    goldenCoinBank

--- GoldenCoinBank/Main.java
+++ GoldenCoinBank/Main.java
@@ -3,8 +3,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
 
-        Bank clients = new Bank(new HashMap<Client, Account>()
-        {{
+        Bank clients = new Bank(new HashMap<>() {{
             put(new Client("Алексей"), new Account(866352, 647291));
             put(new Client("Владимир"), new Account(164953, 604849, 102742));
             put(new Client("Всеволод"), new Account(872450));
